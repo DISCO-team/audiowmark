a944f67fb50cf0f40590bba72810ce7bc4b02e82
Kamil Sokołowski <sokolowski.k@outlook.com>
2023-10-28 01:59:29 +0200

    Convert to string

--- src/mpegts.cc
+++ src/mpegts.cc
@@ -165,13 +165,13 @@ TSWriter::process (const string& inname, const string& outname)
   if (!infile)
     {
       error ("audiowmark: unable to open %s for reading\n", inname.c_str());
-      return Error (std::strerror_s (errno));
+      return Error (std::string (errno));
     }
 
   if (!outfile)
     {
       error ("audiowmark: unable to open %s for writing\n", outname.c_str());
-      return Error (std::strerror_s (errno));
+      return Error (std::string (errno));
     }
 
   while (!feof (infile))
